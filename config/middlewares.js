c7103c205204c42400f99b4cab413cd4b6102355
estluced <estluced@gmail.com>
2023-12-17 13:49:16 +0200

    ini

---
+++ config/middlewares.js
@@ -0,0 +1,22 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb",
+      jsonLimit: "256mb",
+      textLimit: "256mb",
+      formidable: {
+        maxFileSize: 5000 * 1024 * 1024,
+      },
+    },
+  },
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
